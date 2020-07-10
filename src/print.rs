62225b0ef5a789f56b65c9d8b092607fddedd5a9
Nelson Earle <nelson.earle137@gmail.com>
2020-07-10 09:40:13 -0500

    Printer use Write trait instead of print*!()

--- src/print.rs
+++ src/print.rs
@@ -1,8 +1,9 @@
 use std::io::{self, Write};
 
-use crate::util::ends_with_eol;
+use crate::util::{die, ends_with_eol};
 
 pub struct Printer {
+    writer: Box<dyn Write>,
     lines_printed: usize,
     has_final_eol: bool,
 }
@@ -10,6 +11,7 @@ pub struct Printer {
 impl Printer {
     pub fn new() -> Self {
         Self {
+            writer: Box::new(io::stdout()),
             lines_printed: 0,
             has_final_eol: true,
         }
@@ -20,14 +22,18 @@ impl Printer {
         while self.lines_printed > 0 {
             if self.has_final_eol {
                 // Go up one line
-                print!("\x1b[1A");
+                self.writer
+                    .write_all(b"\x1b[1A")
+                    .unwrap_or_else(|err| die(err.to_string()));
             } else {
                 // All lines printed before the last have an EOL by definition
                 self.has_final_eol = true;
             }
 
             // Clear line
-            print!("\r\x1b[K");
+            self.writer
+                .write_all(b"\r\x1b[K")
+                .unwrap_or_else(|err| die(err.to_string()));
 
             self.lines_printed -= 1;
         }
@@ -40,18 +46,24 @@ impl Printer {
         // Print data
         let lines: Vec<_> = data.lines().collect();
         for (i, line) in lines.iter().enumerate() {
-            print!("{}", line);
+            self.writer
+                .write_all(line.as_bytes())
+                .unwrap_or_else(|err| die(err.to_string()));
             if i < lines.len() - 1 || has_final_eol {
-                println!();
+                self.writer
+                    .write_all(b"\n")
+                    .unwrap_or_else(|err| die(err.to_string()));
             }
             self.lines_printed += 1;
         }
 
         // Only update variable if some data was printed (data != "")
-        if lines.len() > 0 {
+        if !lines.is_empty() {
             self.has_final_eol = has_final_eol;
         }
 
-        io::stdout().flush().unwrap();
+        self.writer
+            .flush()
+            .unwrap_or_else(|err| die(err.to_string()));
     }
 }

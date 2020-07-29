8a5c7964043c089be6205d1ff81cf7de787e816e
Nelson Earle <Nelson.Earle@vu.com>
2020-07-29 17:20:22 -0500

    reimplement subcmd live as class

--- src/live/cli.rs
+++ src/live/cli.rs
@@ -37,17 +37,27 @@ pub struct LiveCli {
 }
 
 impl SubCmd for LiveCli {
-    fn run(mut self) {
-        let mut code = if let Some(path) = &self.infile {
-            let script = read_script(&path).unwrap_or_else(|e| die(e));
-            Field::from(&String::from_utf8_lossy(&script))
-        } else {
-            Field::new()
-        };
+    fn run(self) {
+        Live::new(self.width, self.ascii_values, self.infile).run();
+    }
+}
 
+struct Live {
+    window: Window,
+    width: Option<u32>,
+    ascii_values: bool,
+    infile: Option<PathBuf>,
+    code: Field,
+    frame_delay: Duration,
+}
+
+impl Live {
+    fn new(
+        width: Option<u32>,
+        ascii_values: bool,
+        infile: Option<PathBuf>,
+    ) -> Self {
         let window = initscr();
-        let frame_delay = Duration::from_millis(10);
-
         window.keypad(true);
         window.nodelay(true);
         raw();
@@ -61,180 +71,201 @@ impl SubCmd for LiveCli {
             Style::StatusErr.init(COLOR_RED, COLOR_BLACK);
         }
 
-        draw(&window, &code, self.ascii_values);
+        let code = if let Some(path) = &infile {
+            let script = read_script(&path).unwrap_or_else(|e| die(e));
+            Field::from(&String::from_utf8_lossy(&script))
+        } else {
+            Field::new()
+        };
+
+        Self {
+            window,
+            width,
+            ascii_values,
+            infile,
+            code,
+            frame_delay: Duration::from_millis(10),
+        }
+    }
+
+    fn run(&mut self) {
+        self.draw();
 
         loop {
-            sleep(frame_delay);
+            sleep(self.frame_delay);
 
-            let input = match window.getch() {
+            let input = match self.window.getch() {
                 Some(i) => i,
                 None => continue,
             };
 
             match input {
-                KeyEnter => code.enter(),
+                KeyEnter => self.code.enter(),
 
                 // Characters and incorrect control keys
                 Character(c) => {
                     match c {
                         // Enter
-                        '\r' => code.enter(),
+                        '\r' => self.code.enter(),
                         // ^C
                         '\u{3}' => break,
                         // ^A
                         '\u{1}' => self.ascii_values ^= true,
                         // Backspace
-                        '\u{8}' => code.backspace(),
+                        '\u{8}' => self.code.backspace(),
                         // Other
-                        _ => code.insert(c),
+                        _ => self.code.insert(c),
                     }
                 }
 
                 // Arrow keys
-                KeyLeft => code.cursor_left(),
-                KeyRight => code.cursor_right(),
-                KeyUp => code.cursor_up(),
-                KeyDown => code.cursor_down(),
+                KeyLeft => self.code.cursor_left(),
+                KeyRight => self.code.cursor_right(),
+                KeyUp => self.code.cursor_up(),
+                KeyDown => self.code.cursor_down(),
 
                 // Control keys
-                KeyBackspace => code.backspace(),
-                KeyDC => code.delete(),
-                KeyHome => code.cursor_home(),
-                KeyEnd => code.cursor_end(),
-                KeyPPage => code.cursor_top(),
-                KeyNPage => code.cursor_bottom(),
+                KeyBackspace => self.code.backspace(),
+                KeyDC => self.code.delete(),
+                KeyHome => self.code.cursor_home(),
+                KeyEnd => self.code.cursor_end(),
+                KeyPPage => self.code.cursor_top(),
+                KeyNPage => self.code.cursor_bottom(),
 
                 // Terminal resize
                 KeyResize => {
                     resize_term(0, 0);
-                    window.clear();
+                    self.window.clear();
                 }
 
                 // Other
                 _ => (),
             }
 
-            draw(&window, &code, self.ascii_values);
+            self.draw();
         }
 
         endwin();
     }
-}
 
-fn sanitize_output(data: &str) -> String {
-    data.chars()
-        .filter(|c| c.is_ascii_whitespace() || c.is_ascii_graphic())
-        .collect()
-}
+    fn draw(&self) {
+        let (height, width) = self.window.get_max_yx();
+        const STATUS_OK: &str = "ok";
+        let mut status = Ok(STATUS_OK.to_string());
 
-fn draw(window: &Window, code: &Field, ascii_values: bool) {
-    let (height, width) = window.get_max_yx();
-
-    const STATUS_OK: &str = "ok";
-    let mut status = Ok(STATUS_OK.to_string());
-
-    // Run the script
-    let mut interpreter = Interpreter::new(code.text().as_bytes(), "");
-    while let Some(frame) = interpreter.next() {
-        if let Err(err) = frame {
-            status = Err(err);
-            break;
+        // Run the script
+        let mut interpreter =
+            Interpreter::new(self.code.text().as_bytes(), "");
+        while let Some(frame) = interpreter.next() {
+            if let Err(err) = frame {
+                status = Err(err);
+                break;
+            }
         }
+
+        // One character is lost on either side from the border
+        let mut chunks = interpreter.tape.chunks(width - 2);
+        let n_chunks = chunks.len();
+
+        let output: String = interpreter
+            .output
+            .chars()
+            .filter(|c| c.is_ascii_whitespace() || c.is_ascii_graphic())
+            .collect();
+        let output_lines = output.lines().count();
+
+        // Print status
+        self.window.mv(1, 1);
+        let (color, msg) = match status {
+            Ok(msg) => (Style::StatusOk.get(), msg),
+            Err(msg) => (Style::StatusErr.get(), msg),
+        };
+        self.window.attron(color);
+        self.window.attron(A_BOLD);
+        self.window.printw("Status: ");
+        self.window.printw(msg);
+        self.window.attroff(A_BOLD);
+        self.window.attroff(color);
+        self.window.clrtoeol();
+
+        // Print tape
+        self.window.mv(2, 0);
+        chunks.nc_display(&self.window, " ", self.ascii_values);
+
+        let code_y = (3 + n_chunks * 3) as i32;
+        let code_x = 1;
+
+        // Print code
+        self.window.mv(code_y, 0);
+        for line in self.code.lines() {
+            self.window.printw(" ");
+            self.window.printw(line);
+            self.window.printw("\n");
+        }
+
+        // Print output
+        self.window.mv(height - output_lines as i32 - 2, 0);
+        for line in output.lines() {
+            self.window.printw(" ");
+            self.window.printw(line);
+            self.window.printw("\n");
+        }
+
+        self.draw_ui(n_chunks, output_lines);
+
+        self.window.refresh();
+
+        // Move window cursor to cursor position in code
+        let code_cursor = self.code.cursor();
+        let y = code_y + code_cursor.0 as i32;
+        let x = code_x + code_cursor.1 as i32;
+        self.window.mv(y, x);
     }
 
-    // One character is lost on either side from the border
-    let mut chunks = interpreter.tape.chunks(width - 2);
-    let n_chunks = chunks.len();
+    fn draw_ui(&self, n_chunks: usize, output_lines: usize) {
+        let (height, width) = self.window.get_max_yx();
+        let print_horizontal =
+            || (2..width).map(|_| self.window.printw("─")).last();
 
-    let code_cursor = code.cursor();
+        // Top
+        self.window.mvprintw(0, 0, "┌");
+        print_horizontal();
+        self.window.printw("┐");
 
-    let output = sanitize_output(&interpreter.output);
-    let output_lines = output.lines().count();
+        // Left and right
+        for y in 1..height - 2 {
+            self.window.mvprintw(y, 0, "│");
+            self.window.mvprintw(y, width - 1, "│");
+        }
 
-    // Print status
-    window.mv(1, 1);
-    let (color, msg) = match status {
-        Ok(msg) => (Style::StatusOk.get(), msg),
-        Err(msg) => (Style::StatusErr.get(), msg),
-    };
-    window.attron(color);
-    window.attron(A_BOLD);
-    window.printw("Status: ");
-    window.printw(msg);
-    window.attroff(A_BOLD);
-    window.attroff(color);
-    window.clrtoeol();
+        // Bottom
+        self.window.printw("└");
+        print_horizontal();
+        self.window.printw("┘");
 
-    // Print tape
-    window.mv(2, 0);
-    chunks.nc_display(&window, " ", ascii_values);
+        // Controls
+        const CONTROLS: [[&str; 2]; 2] =
+            [["^C", "!Quit"], ["^A", "Toggle ASCII"]];
+        CONTROLS.iter().for_each(|[map, hint]| {
+            self.window.attron(Style::ControlHint.get());
+            self.window.printw(map);
+            self.window.attroff(Style::ControlHint.get());
+            self.window.printw(":");
+            self.window.printw(hint);
+            self.window.printw("  ");
+        });
+        self.window.clrtoeol();
 
-    let code_y = (3 + n_chunks * 3) as i32;
-    let code_x = 1;
+        // Divider 1
+        let divider_y = (2 + n_chunks * 3) as i32;
+        self.window.mvprintw(divider_y, 0, "├");
+        print_horizontal();
+        self.window.printw("┤");
 
-    // Print code
-    window.mv(code_y, 0);
-    for line in code.lines() {
-        window.printw(" ");
-        window.printw(line);
-        window.printw("\n");
+        // Divider 2
+        let divider_y = height as i32 - 3 - output_lines as i32;
+        self.window.mvprintw(divider_y, 0, "├");
+        print_horizontal();
+        self.window.printw("┤");
     }
-
-    // Print output
-    window.mv(height - output_lines as i32 - 2, 0);
-    for line in output.lines() {
-        window.printw(" ");
-        window.printw(line);
-        window.printw("\n");
-    }
-
-    draw_ui(&window, n_chunks, output_lines);
-
-    window.refresh();
-    window.mv(code_y + code_cursor.0 as i32, code_x + code_cursor.1 as i32);
-}
-
-fn draw_ui(window: &Window, n_chunks: usize, output_lines: usize) {
-    let (height, width) = window.get_max_yx();
-    let print_horizontal = || (2..width).map(|_| window.printw("─")).last();
-
-    // Top
-    window.mvprintw(0, 0, "┌");
-    print_horizontal();
-    window.printw("┐");
-
-    // Left and right
-    for y in 1..height - 2 {
-        window.mvprintw(y, 0, "│");
-        window.mvprintw(y, width - 1, "│");
-    }
-
-    // Bottom
-    window.printw("└");
-    print_horizontal();
-    window.printw("┘");
-
-    // Controls
-    const CONTROLS: [[&str; 2]; 2] = [["^C", "!Quit"], ["^A", "Toggle ASCII"]];
-    CONTROLS.iter().for_each(|[map, hint]| {
-        window.attron(Style::ControlHint.get());
-        window.printw(map);
-        window.attroff(Style::ControlHint.get());
-        window.printw(":");
-        window.printw(hint);
-        window.printw("  ");
-    });
-    window.clrtoeol();
-
-    // Divider 1
-    let divider_y = (2 + n_chunks * 3) as i32;
-    window.mvprintw(divider_y, 0, "├");
-    print_horizontal();
-    window.printw("┤");
-
-    // Divider 2
-    let divider_y = height as i32 - 3 - output_lines as i32;
-    window.mvprintw(divider_y, 0, "├");
-    print_horizontal();
-    window.printw("┤");
 }

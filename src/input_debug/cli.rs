7c4ef2d742db2023d195b61b69dd14e092bdce9a
Nelson Earle <nelson.earle137@gmail.com>
2021-05-10 16:49:46 -0500

    subcmd input_debug: add CLI flag for enable mouse

--- src/input_debug/cli.rs
+++ src/input_debug/cli.rs
@@ -7,16 +7,19 @@ use crate::{
 
 use super::app::App;
 
-const ABOUT: &str = "Live scripting playground";
+const ABOUT: &str = "User input debugger";
+const MOUSE_HELP: &str = "Whether to show mouse events";
 
 #[derive(Debug, StructOpt)]
 #[structopt(about=ABOUT)]
 pub struct InputDebugCli {
+    #[structopt(short="m", long, help=MOUSE_HELP)]
+    enable_mouse: bool,
 }
 
 impl SubCmd for InputDebugCli {
     fn run(self) {
-        App::new()
+        App::new(self.enable_mouse)
             .unwrap_or_else(|e| die(e.to_string()))
             .run()
             .unwrap_or_else(|e| die(e.to_string()));

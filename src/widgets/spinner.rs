a8ed6eddde3d9bc21df9bd6e0d4e1eb08545a4a5
Nelson Earle <nelson.earle137@gmail.com>
2023-10-03 22:13:59 -0500

    refactor spinner widget

--- src/widgets/spinner.rs
+++ src/widgets/spinner.rs
@@ -3,30 +3,19 @@ use ratatui::{
     widgets::{Paragraph, Widget},
 };
 
-const SPINNER: &str = "│╱─╲";
+const SPINNER: [&str; 4] = ["│", "╱", "─", "╲"];
 
 #[derive(Clone, Copy, Default)]
 pub struct Spinner(usize);
 
 impl Spinner {
-    fn state_boundaries() -> impl Iterator<Item = usize> {
-        (0..=SPINNER.len()).filter(|i| SPINNER.is_char_boundary(*i))
-    }
-
     pub fn tick(&mut self) {
-        let n_states = Self::state_boundaries().count() - 1;
-        self.0 = (self.0 + 1) % n_states;
-    }
-
-    fn get(&self) -> &'static str {
-        let mut indexes = Vec::with_capacity(2);
-        indexes.extend(Self::state_boundaries().skip(self.0).take(2));
-        &SPINNER[indexes[0]..indexes[1]]
+        self.0 = (self.0 + 1) % SPINNER.len();
     }
 }
 
 impl Widget for Spinner {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        Paragraph::new(self.get()).render(area, buf);
+        Paragraph::new(SPINNER[self.0]).render(area, buf);
     }
 }

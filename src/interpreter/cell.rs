f9e772a13ad29f8bdc0a83d14556e858f3e508c5
Nelson Earle <nelson.earle137@gmail.com>
2023-10-06 01:29:24 -0500

    add interpreter cell tests

--- src/interpreter/cell.rs
+++ src/interpreter/cell.rs
@@ -117,3 +117,45 @@ impl Widget for CellDisplay {
         Paragraph::new(self.display_bottom()).render(bottom_area, buf);
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn new_initializes_to_0() {
+        let cell = Cell::new();
+        assert_eq!(cell.0 .0, 0);
+    }
+
+    #[test]
+    fn inc_increases_value_by_1() {
+        let value = fastrand::u8(..u8::MAX);
+        let mut cell = Cell(Wrapping(value));
+        cell.inc();
+        assert_eq!(cell.0 .0, value + 1);
+    }
+
+    #[test]
+    fn dec_decreases_value_by_1() {
+        let value = fastrand::u8(1..);
+        let mut cell = Cell(Wrapping(value));
+        cell.dec();
+        assert_eq!(cell.0 .0, value - 1);
+    }
+
+    #[test]
+    fn value_returns_the_value() {
+        let value = fastrand::u8(..);
+        let cell = Cell(Wrapping(value));
+        assert_eq!(cell.value(), value);
+    }
+
+    #[test]
+    fn set_updates_the_value() {
+        let value = fastrand::u8(..);
+        let mut cell = Cell::new();
+        cell.set(value);
+        assert_eq!(cell.0 .0, value);
+    }
+}

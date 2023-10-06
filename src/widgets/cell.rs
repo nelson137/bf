0d0e47870ecbf3e6bb0bd7842f2c164d2a829818
Nelson Earle <nelson.earle137@gmail.com>
2023-10-06 09:40:30 -0500

    add tape cell widget tests

--- src/widgets/cell.rs
+++ src/widgets/cell.rs
@@ -13,6 +13,7 @@ use crate::util::tui::{
     TAPE_HORIZONTAL_BORDER_BOTTOM, TAPE_HORIZONTAL_BORDER_TOP,
 };
 
+#[derive(Default)]
 pub struct CellWidget {
     pub value: u8,
     pub left_cap: bool,
@@ -86,8 +87,152 @@ impl Widget for CellWidget {
             Span::from(display_value)
         };
         Paragraph::new(Line::from(vec![left_border, value, right_border]))
-        .render(middle_area, buf);
+            .render(middle_area, buf);
 
         Paragraph::new(self.display_bottom()).render(bottom_area, buf);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use test_case::test_case;
+
+    use super::*;
+
+    mod util {
+        use ratatui::{style::Style, Terminal};
+
+        use crate::util::test::{terminal, MyTestBackend};
+
+        use super::super::*;
+
+        pub fn num_cell_with_value(value: u8) -> CellWidget {
+            CellWidget {
+                value,
+                ..Default::default()
+            }
+        }
+
+        pub fn ascii_cell_with_value(value: u8) -> CellWidget {
+            CellWidget {
+                ascii: true,
+                value,
+                ..Default::default()
+            }
+        }
+
+        pub fn terminal_for_cell() -> (Terminal<MyTestBackend>, MyTestBackend)
+        {
+            terminal(5, 3)
+        }
+
+        pub fn buf_for_cell(
+            left_cap: bool,
+            right: Option<bool>,
+            highlighted: bool,
+        ) -> Buffer {
+            let top_left = if left_cap { "┌" } else { "┬" };
+            let top_right_char =
+                |capped: bool| if capped { "┐" } else { "┬" };
+            let top_right = right.map(top_right_char).unwrap_or(" ");
+            let top = String::with_capacity(5) + top_left + "───" + top_right;
+
+            let middle = String::with_capacity(5)
+                + "│ 0 "
+                + right.map(|_| "│").unwrap_or(" ");
+
+            let bottom_left = if left_cap { "└" } else { "┴" };
+            let bottom_right_char =
+                |capped: bool| if capped { "┘" } else { "┴" };
+            let bottom_right = right.map(bottom_right_char).unwrap_or(" ");
+            let bottom =
+                String::with_capacity(5) + bottom_left + "───" + bottom_right;
+
+            let mut buffer = Buffer::with_lines(vec![top, middle, bottom]);
+
+            if highlighted {
+                buffer.set_string(1, 1, " 0 ", Style::default().reversed());
+            }
+
+            buffer
+        }
+
+        pub fn render_cell(
+            term: &mut Terminal<MyTestBackend>,
+            widget: CellWidget,
+        ) {
+            term.draw(|f| f.render_widget(widget, f.size())).unwrap();
+        }
+    }
+    use util::*;
+
+    #[test_case(4, " 4 " ; "4")]
+    #[test_case(42, "42 " ; "42")]
+    #[test_case(255, "255" ; "255")]
+    fn display_numerical_value(value: u8, expected_str: &str) {
+        let widget = num_cell_with_value(value);
+        assert_eq!(widget.display_value(), expected_str);
+    }
+
+    #[test_case(b'\0', r"\0 " ; "null")]
+    #[test_case(b'\t', r"\t " ; "tab")]
+    #[test_case(b'\r', r"\r " ; "carriage return")]
+    #[test_case(b'\n', r"\n " ; "newline")]
+    #[test_case(b' ', "' '" ; "space")]
+    #[test_case(b'A', " A " ; "capital a")]
+    #[test_case(127, "127" ; "127")]
+    fn display_ascii_value(value: u8, expected_str: &str) {
+        let widget = ascii_cell_with_value(value);
+        assert_eq!(widget.display_value(), expected_str);
+    }
+
+    #[test]
+    fn render_left_cap() {
+        let (mut term, backend) = terminal_for_cell();
+
+        let widget = CellWidget {
+            left_cap: true,
+            ..Default::default()
+        };
+
+        let expected_buf = buf_for_cell(true, None, false);
+
+        render_cell(&mut term, widget);
+
+        backend.get().assert_buffer(&expected_buf);
+    }
+
+    #[test_case(None ; "no cap")]
+    #[test_case(Some(false) ; "uncapped")]
+    #[test_case(Some(true) ; "capped")]
+    fn render_right(right_border_cap: Option<bool>) {
+        let (mut term, backend) = terminal_for_cell();
+
+        let widget = CellWidget {
+            right_border_cap,
+            ..Default::default()
+        };
+
+        let expected_buf = buf_for_cell(false, right_border_cap, false);
+
+        render_cell(&mut term, widget);
+
+        backend.get().assert_buffer(&expected_buf);
+    }
+
+    #[test]
+    fn render_highlight() {
+        let (mut term, backend) = terminal_for_cell();
+
+        let widget = CellWidget {
+            is_highlighted: true,
+            ..Default::default()
+        };
+
+        let expected_buf = buf_for_cell(false, None, true);
+
+        render_cell(&mut term, widget);
+
+        backend.get().assert_buffer(&expected_buf);
     }
 }

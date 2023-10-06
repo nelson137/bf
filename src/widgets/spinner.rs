8112c83fa139fc1552af4e4af234679b7baeb6f7
Nelson Earle <nelson.earle137@gmail.com>
2023-10-06 09:59:40 -0500

    add spinner widget tests

--- src/widgets/spinner.rs
+++ src/widgets/spinner.rs
@@ -19,3 +19,57 @@ impl Widget for Spinner {
         Paragraph::new(SPINNER[self.0]).render(area, buf);
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    mod util {
+        use ratatui::Terminal;
+
+        use crate::util::test::{terminal, MyTestBackend};
+
+        use super::super::*;
+
+        pub fn terminal_for_cell() -> (Terminal<MyTestBackend>, MyTestBackend)
+        {
+            terminal(1, 1)
+        }
+
+        pub fn render_cell(
+            term: &mut Terminal<MyTestBackend>,
+            widget: Spinner,
+        ) {
+            term.draw(|f| f.render_widget(widget, f.size())).unwrap();
+        }
+    }
+    use util::*;
+
+    #[test]
+    fn tick_increases_index_by_1() {
+        let index = fastrand::usize(..(SPINNER.len() - 1));
+        let mut spinner = Spinner(index);
+        spinner.tick();
+        assert_eq!(spinner.0, index + 1);
+    }
+
+    #[test]
+    fn tick_wraps_to_0() {
+        let mut spinner = Spinner(SPINNER.len() - 1);
+        spinner.tick();
+        assert_eq!(spinner.0, 0);
+    }
+
+    #[test]
+    fn renders_a_frame() {
+        let (mut term, backend) = terminal_for_cell();
+
+        let index = fastrand::usize(0..SPINNER.len());
+        let expected_buf = Buffer::with_lines(vec![SPINNER[index]]);
+        let spinner = Spinner(index);
+
+        render_cell(&mut term, spinner);
+
+        backend.get().assert_buffer(&expected_buf);
+    }
+}

15cfba002ef6a98c311af9cbb50e3045a308721f
Nelson Earle <nelson.earle137@gmail.com>
2023-10-08 17:48:38 -0500

    inline scroll: use scrolled paragraph

--- src/commands/inline_scroll/mod.rs
+++ src/commands/inline_scroll/mod.rs
@@ -1,5 +1,4 @@
 use std::{
-    collections::VecDeque,
     io::stdout,
     time::{SystemTime, UNIX_EPOCH},
 };
@@ -44,7 +43,7 @@ fn run(args: InlineScrollCli) -> Result<()> {
         .flat_map(|l| l.chars().filter(|c| !c.is_whitespace()))
         .collect();
 
-    let mut output: VecDeque<String> = Default::default();
+    let mut output: Vec<String> = Default::default();
     let mut id = 0;
 
     enable_raw_mode()?;
@@ -65,23 +64,17 @@ fn run(args: InlineScrollCli) -> Result<()> {
                     BfEvent::Tick => {
                         let now =
                             SystemTime::now().duration_since(UNIX_EPOCH)?;
-                        output.push_back(format!(
+                        output.push(format!(
                             "{ts:.03} {script} {id}",
                             ts = now.as_millis() as f64 / 1000.0,
                         ));
                         id += 1;
 
+                        let lines = output.iter().map(String::as_str);
                         let len = output.len() as u16;
                         let size = terminal.size()?;
-                        if len > size.height {
-                            let excess_len = len - size.height;
-                            output.pop_front();
-                            terminal.backend_mut().append_lines(excess_len)?;
-                            terminal.clear()?;
-                        }
-
-                        let lines = output.iter().map(String::as_str);
-                        terminal.draw(|frame| draw(frame, lines))?;
+                        let scroll = len.saturating_sub(size.height);
+                        terminal.draw(|frame| draw(frame, lines, scroll))?;
                     }
                     BfEvent::Input(Event::Key(key_evt)) => {
                         if event_matches!(key_evt, KeyCode::Esc) {
@@ -100,9 +93,14 @@ fn run(args: InlineScrollCli) -> Result<()> {
     ret
 }
 
-fn draw<'s>(frame: &mut Frame, lines: impl Iterator<Item = &'s str>) {
+fn draw<'s>(
+    frame: &mut Frame,
+    lines: impl Iterator<Item = &'s str>,
+    scroll: u16,
+) {
     let area = frame.size();
     let lines: Vec<Line> = lines.map(Line::from).collect();
     let text = Text::from(lines);
-    frame.render_widget(Paragraph::new(text), area);
+    let widget = Paragraph::new(text).scroll((scroll, 0));
+    frame.render_widget(widget, area);
 }

6e03027798ae910687dff6e1a44a3af930359d15
Nelson Earle <nelson.earle137@gmail.com>
2026-07-03 14:25:58 -0500

    upgrade minor deps

--- bf_cli/benches/file_hash.rs
+++ bf_cli/benches/file_hash.rs
@@ -1,6 +1,6 @@
-use criterion::{
-    black_box, criterion_group, criterion_main, BenchmarkId, Criterion,
-};
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 pub type Sha1Digest = [u8; 20];
 
